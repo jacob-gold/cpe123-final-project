60d48f9da9655ceb8140559943d8049e1a4430ae
Horse0160 <zuzia.dominik@gmail.com>
2018-12-03 10:38:04 -0800

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -2,5 +2,59 @@ function setup() {
 	createCanvas(400, 400);
 }
 function draw() {
-	
+	dude();	
+}
+
+function dude(){
+   push();
+      translate(200,200);
+      //face
+      fill(190,140,90);
+      ellipse(0,0, 30,40);
+      //hat
+      fill(0,200,0);
+      triangle(20,-10, -5, -30, -15, -20);
+      //feather
+      for(var angle=0; angle<PI; angle+= PI/100){
+         var x0 = -20 + -30*cos(angle);
+         var y0 = -15 + -10*sin(angle);
+
+         var x1 = -20 + -20*cos(angle);
+         var y1 = -20 + -10*sin(angle);
+
+         stroke(255,0,0);
+         line(x0,y0, x1,y1);
+      }
+      //hat
+      stroke(0);
+      fill(0,100,0);
+      triangle(-20,-10, 20,-10, -15,-24);
+      fill(200,0,0);     
+      //torso
+      fill(0,200,0);
+      rect(-10,20, 20, 50);
+      //lute/guitar
+      fill(90,70,40);
+      ellipse(15,50, 10, 30);
+      quad(15,45, 30,30, 30,35, 15, 55);
+      //arm
+      fill(0,200,0);
+      push();
+         translate(0,30);
+         rect(0,0, 20,10);
+      pop();
+      //legs
+      fill(0);
+      push();
+         translate(0,70);
+         rect(0,0,7,40);
+      pop();
+      push();
+         translate(-10,70);
+         rect(0,0,7,40);
+      pop();
+      //cape
+      fill(0,100,0);
+      quad(10,20, -10,20, -20,80, -10,90);
+   pop();
 }
